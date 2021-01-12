f03bd28757750d106591937c16fd0c7a9be62137
Stephan Truchsess <stephantruchsess@gmail.com>
2021-01-12 13:29:38 -0500

    created loop for printing string as animation

---
+++ typewriter.js
@@ -0,0 +1,15 @@
+const sentence = "hello there from lighthouse labs";
+
+
+let delay = 0;
+
+for (const char of sentence) {
+    setTimeout(() => {
+      process.stdout.write(char)
+    }, delay)
+    delay += 50
+}
+
+setTimeout(() => {
+  process.stdout.write('\n')
+}, delay);
