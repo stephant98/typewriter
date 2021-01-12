253df29c3f0d12a73b9bb573051ad39a99af8a7e
Stephan Truchsess <stephantruchsess@gmail.com>
2021-01-12 17:40:36 -0500

    linted file

--- typewriter.js
+++ typewriter.js
@@ -4,12 +4,12 @@ const sentence = "hello there from lighthouse labs";
 let delay = 0;
 
 for (const char of sentence) {
-    setTimeout(() => {
-      process.stdout.write(char)
-    }, delay)
-    delay += 50
+  setTimeout(() => {
+    process.stdout.write(char);
+  }, delay);
+  delay += 50;
 }
 
 setTimeout(() => {
-  process.stdout.write('\n')
+  process.stdout.write('\n');
 }, delay);
